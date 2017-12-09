921d14ffdd7b6711f5fa7c2c927806fe0b4c48d7
Vladimir Kamarzin <vvk@vvk.pp.ru>
2017-12-10 00:34:19 +0500

    Add script to estimate upvote addition to post payout

---
+++ estimate_upvote.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+
+import sys
+import json
+import argparse
+import logging
+import yaml
+import re
+
+from piston import Steem
+from piston.amount import Amount
+
+import functions
+
+log = logging.getLogger('functions')
+
+
+def main():
+
+    parser = argparse.ArgumentParser(
+            description='Use this script to estimate potential upvote profit',
+            epilog='Report bugs to: https://github.com/bitfag/golos-scripts/issues')
+    parser.add_argument('-d', '--debug', action='store_true',
+            help='enable debug output'),
+    parser.add_argument('-c', '--config', default='./common.yml',
+            help='specify custom path for config file')
+    parser.add_argument('-a', '--account',
+            help='specify account which upvote should be estimated')
+    parser.add_argument('-p', '--percent', default=100, type=float,
+            help='specify upvote percent')
+    parser.add_argument('url',
+            help='post id in format @author/article or full url')
+    args = parser.parse_args()
+
+    # create logger
+    if args.debug == True:
+        log.setLevel(logging.DEBUG)
+    else:
+        log.setLevel(logging.INFO)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter("%(levelname)s: %(message)s")
+    handler.setFormatter(formatter)
+    log.addHandler(handler)
+
+    # parse config
+    with open(args.config, 'r') as ymlfile:
+        conf = yaml.load(ymlfile)
+
+    golos = Steem(node=conf['nodes_old'], keys=conf['keys'])
+
+    # extract author and post permlink from args.url
+    p = re.search('@(.*?)/([^/]+)', args.url)
+    if p == None:
+        log.critical('Wrong post id specified')
+        sys.exit(1)
+    else:
+        author = p.group(1)
+        log.debug('author: {}'.format(author))
+
+        post_permlink = p.group(2)
+        log.debug('permlink: {}'.format(post_permlink))
+
+
+    post = functions.get_post_content(golos, author, post_permlink)
+    if not post:
+        log.critical('could not find post in blockchain')
+        sys.exit(1)
+
+    # current post rshares
+    net_rshares = int(post['net_rshares'])
+
+    # current pending_payout_value, GBG
+    current_pending_payout_value = post['pending_payout_value']
+    log.info('current pending_payout_value: {}'.format(current_pending_payout_value))
+
+    # estimate current expected author reward
+    author_payout_gp, author_payout_gbg, author_payout_golos = functions.estimate_author_payout(golos, current_pending_payout_value)
+    log.info('estimated author payout: {:.3f} GBG, {:.3f} GOLOS, {:.3f} GP'.format(
+        author_payout_gbg, author_payout_golos, author_payout_gp))
+
+    rshares = functions.calc_rshares(golos, args.account, args.percent)
+    new_rshares = net_rshares + rshares
+
+    new_payout = functions.calc_payout(golos, new_rshares)
+    new_payout_gbg = functions.convert_golos_to_gbg(golos, new_payout, price_source='median')
+    new_payout_gbg = Amount('{} GBG'.format(new_payout_gbg))
+    log.info('new pending_payout_value: {}'.format(new_payout_gbg))
+
+    payout_diff = new_payout_gbg.amount - current_pending_payout_value.amount
+    log.info('pending_payout diff: {:.3f}'.format(payout_diff))
+
+    # estimate new author reward
+    author_payout_gp, author_payout_gbg, author_payout_golos = functions.estimate_author_payout(golos, new_payout_gbg)
+    log.info('new estimated author payout: {:.3f} GBG, {:.3f} GOLOS, {:.3f} GP'.format(
+        author_payout_gbg, author_payout_golos, author_payout_gp))
+
+
+if __name__ == '__main__':
+    main()
