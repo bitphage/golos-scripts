4468a87926fa224f1c17baf276469f1c7d0808bf
Vladimir Kamarzin <vvk@vvk.pp.ru>
2020-04-17 14:10:22 +0500

    Refactor get_balance.py

--- get_balance.py
+++ get_balance.py
@@ -1,41 +1,30 @@
 #!/usr/bin/env python
 
-import argparse
-import yaml
-
-from golos import Steem
+import click
 from golos.account import Account
-from golos.converter import Converter
-from pprint import pprint
+
+from golosscripts.decorators import common_options, helper
 
 
-def main():
+@click.command()
+@common_options
+@helper
+@click.argument('account')
+@click.pass_context
+def main(ctx, account):
+    """Show account balances."""
 
-    parser = argparse.ArgumentParser(
-        description='show user balances', epilog='Report bugs to: https://github.com/bitfag/golos-scripts/issues'
-    )
-    parser.add_argument('account', help='account name')
-    parser.add_argument('-c', '--config', default='./common.yml', help='specify custom path for config file')
-    args = parser.parse_args()
+    account = Account(account, steemd_instance=ctx.helper)
+    balance = account.get_balances()
+    gp = ctx.helper.converter.vests_to_sp(balance['total']['GESTS'])
 
-    # parse config
-    with open(args.config, 'r') as ymlfile:
-        conf = yaml.safe_load(ymlfile)
-
-    golos = Steem(nodes=conf['nodes_old'], no_broadcast=True)
-
-    a = Account(args.account, steemd_instance=golos)
-    b = a.get_balances()
-    cv = Converter(golos)
-    gp = cv.vests_to_sp(b['total']['GESTS'])
-
-    for asset in b['savings']:
-        print('{:<15}{:>18.3f}'.format('SAVINGS_{}'.format(asset), b['savings'][asset]))
-    for asset in b['total']:
-        print('{:<15}{:>18.3f}'.format('{}:'.format(asset), b['available'][asset]))
+    for asset in balance['savings']:
+        print('{:<15}{:>18.3f}'.format('SAVINGS_{}'.format(asset), balance['savings'][asset]))
+    for asset in balance['total']:
+        print('{:<15}{:>18.3f}'.format('{}:'.format(asset), balance['available'][asset]))
 
     print('{:<15}{:>18.3f}'.format('GP:', gp))
-    print('{:<15}{:>18.3f}'.format('MGESTS:', b['total']['GESTS'] / 1000000))
+    print('{:<15}{:>18.3f}'.format('MGESTS:', balance['total']['GESTS'] / 1000000))
 
 
 if __name__ == '__main__':
