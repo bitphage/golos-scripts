b21efb2268d51e84517abcaab1a1ad0c899edc9f
Vladimir Kamarzin <vvk@vvk.pp.ru>
2020-04-17 14:26:15 +0500

    Refactor get_balance_multi.py

--- get_balance_multi.py
+++ get_balance_multi.py
@@ -1,66 +1,40 @@
 #!/usr/bin/env python
 
-import argparse
-import yaml
-import logging
-
-from golos import Steem
+import click
 from golos.account import Account
-from golos.converter import Converter
-from pprint import pprint
 
-log = logging.getLogger(__name__)
+from golosscripts.decorators import common_options, helper
 
 
-def main():
+@click.command()
+@common_options
+@helper
+@click.option('--no-header', default=False, is_flag=True, help='supress header')
+@click.option('--no-sum', default=False, is_flag=True, help='supress summary output')
+@click.pass_context
+def main(ctx, no_header, no_sum):
+    """Show multiple users balances."""
 
-    parser = argparse.ArgumentParser(
-        description='show multiple users balances',
-        epilog='Report bugs to: https://github.com/bitfag/golos-scripts/issues',
-    )
-    parser.add_argument('-c', '--config', default='./common.yml', help='specify custom path for config file')
-    parser.add_argument('--no-header', action='store_true', help='supress header')
-    parser.add_argument('--no-sum', action='store_true', help='supress summary output')
-    parser.add_argument('-d', '--debug', action='store_true', help='enable debug output'),
-
-    args = parser.parse_args()
-
-    # create logger
-    if args.debug == True:
-        log.setLevel(logging.DEBUG)
-    else:
-        log.setLevel(logging.INFO)
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
-    handler.setFormatter(formatter)
-    log.addHandler(handler)
-
-    # parse config
-    with open(args.config, 'r') as ymlfile:
-        conf = yaml.safe_load(ymlfile)
-
-    golos = Steem(nodes=conf['nodes_old'], no_broadcast=True)
-
-    if not args.no_header:
+    if not no_header:
         print('{:<20} {:>10} {:>11} {:>11}'.format('Account', 'GBG', 'GOLOS', 'GP'))
         print('--------------------')
 
     sum_gbg = float()
     sum_golos = float()
     sum_gp = float()
-    for acc in conf['accs']:
-        a = Account(acc, steemd_instance=golos)
-        b = a.get_balances()
-        cv = Converter(golos)
-        gp = cv.vests_to_sp(b['total']['GESTS'])
+    for acc in ctx.config['accs']:
+        account = Account(acc)
+        balance = account.get_balances()
+        cv = ctx.helper.converter
+        gp = cv.vests_to_sp(balance['total']['GESTS'])
 
-        print('{:<20} {:>10}  {:>10}  {:>10.0f}'.format(acc, b['total']['GBG'], b['total']['GOLOS'], gp))
+        print('{:<20} {:>10}  {:>10}  {:>10.0f}'.format(acc, balance['total']['GBG'], balance['total']['GOLOS'], gp))
 
-        sum_gbg += b['total']['GBG']
-        sum_golos += b['total']['GOLOS']
+        sum_gbg += balance['total']['GBG']
+        sum_golos += balance['total']['GOLOS']
         sum_gp += gp
 
-    if not args.no_sum:
+    if not no_sum:
         print('--------------------')
         print('{:<20} {:>10.3f}  {:>10.3f}  {:>10.0f}'.format('Totals:', sum_gbg, sum_golos, sum_gp))
 
