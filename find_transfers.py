d4928c4412fbe5f98e7836af71c63e86abd9528a
Vladimir Kamarzin <vvk@vvk.pp.ru>
2020-04-19 12:54:52 +0500

    Refactor find_transfers.py

--- find_transfers.py
+++ find_transfers.py
@@ -1,63 +1,41 @@
 #!/usr/bin/env python
 
-import argparse
-import logging
-import yaml
+from pprint import pformat
 
-from datetime import datetime
-from golos import Steem
+import click
 from golos.account import Account
 from golos.amount import Amount
+from golos.utils import parse_time
 
-log = logging.getLogger('functions')
+from golosscripts.decorators import common_options, helper
 
 
-def main():
+@click.command()
+@common_options
+@helper
+@click.option(
+    '-a', '--amount', 'amount_limit', type=float, default=0, help='minimal transfer amount to look for (default: 0)'
+)
+@click.option('-l', '--limit', type=int, default=50, help='limit number of transactions to scan, default: 50')
+@click.argument('account')
+@click.pass_context
+def main(ctx, amount_limit, limit, account):
+    """Scan account history looking for transfers."""
 
-    parser = argparse.ArgumentParser(
-        description='Scan account history looking for transfers',
-        epilog='Report bugs to: https://github.com/bitfag/golos-scripts/issues',
-    )
-    parser.add_argument('-d', '--debug', action='store_true', help='enable debug output'),
-    parser.add_argument('-c', '--config', default='./common.yml', help='specify custom path for config file')
-    parser.add_argument(
-        '-a', '--amount', type=float, default=0, help='minimal transfer amount to look for (default: 0)'
-    )
-    parser.add_argument('-l', '--limit', type=int, default=50, help='limit number of transactions to scan, default: 50')
-    parser.add_argument('account', help='account to scan')
-    args = parser.parse_args()
-
-    # create logger
-    if args.debug is True:
-        log.setLevel(logging.DEBUG)
-    else:
-        log.setLevel(logging.INFO)
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
-    handler.setFormatter(formatter)
-    log.addHandler(handler)
-
-    # parse config
-    with open(args.config, 'r') as ymlfile:
-        conf = yaml.safe_load(ymlfile)
-
-    golos = Steem(nodes=conf['nodes_old'], keys=conf['keys'])
-
-    account = Account(args.account, steemd_instance=golos)
-    history = account.rawhistory(only_ops=['transfer'], limit=args.limit)
+    account = Account(account)
+    history = account.rawhistory(only_ops=['transfer'], limit=limit)
 
     for item in history:
-        # from pprint import pprint
-        # pprint(item)
+        ctx.log.debug(pformat(item))
 
-        timestamp = datetime.strptime(item[1]['timestamp'], '%Y-%m-%dT%H:%M:%S')
-        t_from = item[1]['op'][1]['from']
+        timestamp = parse_time(item[1]['timestamp'])
+        from_ = item[1]['op'][1]['from']
         to = item[1]['op'][1]['to']
         amount = Amount(item[1]['op'][1]['amount'])
         memo = item[1]['op'][1]['memo']
 
-        if amount.amount > args.amount:
-            print('{}: {:<16} -> {:<16}, {}, {}'.format(timestamp, t_from, to, amount, memo))
+        if amount.amount > amount_limit:
+            print('{}: {:<16} -> {:<16}, {}, {}'.format(timestamp, from_, to, amount, memo))
 
 
 if __name__ == '__main__':
