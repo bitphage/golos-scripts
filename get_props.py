8e071b3a4b651f5473f769796b848d0376b13553
Vladimir Kamarzin <vvk@vvk.pp.ru>
2020-04-19 12:59:48 +0500

    Refactor get_props.py

--- get_props.py
+++ get_props.py
@@ -1,44 +1,21 @@
 #!/usr/bin/env python
 
-import argparse
-import logging
-import yaml
 from pprint import pprint
-from golos import Steem
-from golos.converter import Converter
+
+import click
+
+from golosscripts.decorators import common_options, helper
 
 
-log = logging.getLogger('functions')
+@click.command()
+@common_options
+@helper
+@click.pass_context
+def main(ctx):
+    """Show global properties and "steem per mvests"."""
 
-
-def main():
-
-    parser = argparse.ArgumentParser(
-        description='', epilog='Report bugs to: https://github.com/bitfag/golos-scripts/issues'
-    )
-    parser.add_argument('-d', '--debug', action='store_true', help='enable debug output'),
-    parser.add_argument('-c', '--config', default='./common.yml', help='specify custom path for config file')
-    args = parser.parse_args()
-
-    # create logger
-    if args.debug is True:
-        log.setLevel(logging.DEBUG)
-    else:
-        log.setLevel(logging.INFO)
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
-    handler.setFormatter(formatter)
-    log.addHandler(handler)
-
-    # parse config
-    with open(args.config, 'r') as ymlfile:
-        conf = yaml.safe_load(ymlfile)
-
-    golos = Steem(nodes=conf['nodes_old'])
-    pprint(golos.get_dynamic_global_properties())
-
-    cv = Converter(golos)
-    print('steem_per_mvests: {}'.format(cv.steem_per_mvests()))
+    pprint(ctx.helper.get_dynamic_global_properties())
+    print('steem_per_mvests: {}'.format(ctx.helper.converter.steem_per_mvests()))
 
 
 if __name__ == '__main__':
