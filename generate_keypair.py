c766680d4ce7223948a224ea90793c03fa96adad
Vladimir Kamarzin <vvk@vvk.pp.ru>
2020-04-19 12:33:26 +0500

    Refactor generate_keypair.py

--- generate_keypair.py
+++ generate_keypair.py
@@ -1,58 +1,24 @@
 #!/usr/bin/env python
 
-import sys
-import json
-import argparse
-import logging
-import yaml
-import hashlib
+import click
+from golosbase.account import PrivateKey
 
-from binascii import hexlify
-from golos import Steem
-from golosbase.account import PrivateKey, PublicKey
-
-import functions
-
-log = logging.getLogger('functions')
+from golosscripts.decorators import common_options, helper
 
 
-def main():
+@click.command()
+@common_options
+@helper
+@click.pass_context
+def main(ctx):
+    """Generate random keypair."""
 
-    parser = argparse.ArgumentParser(
-        description='This script is for generating a keypair.',
-        epilog='Report bugs to: https://github.com/bitfag/golos-scripts/issues',
-    )
-    parser.add_argument('-d', '--debug', action='store_true', help='enable debug output'),
-    parser.add_argument('-c', '--config', default='./common.yml', help='specify custom path for config file')
-    args = parser.parse_args()
+    wif = PrivateKey()
+    prefix = ctx.helper.steemd.chain_params["prefix"]
+    pubkey = format(wif.pubkey, prefix)
 
-    # create logger
-    if args.debug == True:
-        log.setLevel(logging.DEBUG)
-    else:
-        log.setLevel(logging.INFO)
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
-    handler.setFormatter(formatter)
-    log.addHandler(handler)
-
-    # parse config
-    with open(args.config, 'r') as ymlfile:
-        conf = yaml.safe_load(ymlfile)
-
-    golos = Steem(nodes=conf['nodes_old'], no_broadcast=False, keys=conf['keys'])
-
-    password = functions.generate_password()
-
-    a = bytes(password, 'utf8')
-    s = hashlib.sha256(a).digest()
-
-    privkey = PrivateKey(hexlify(s).decode('ascii'))
-    print('private: {}'.format(str(privkey)))  # we need explicit str() conversion!
-
-    # pubkey with correct prefix
-    key = format(privkey.pubkey, golos.chain_params["prefix"])
-    print('public: {}'.format(key))
+    print(f'private key: {str(wif)}')
+    print(f'public key: {pubkey}')
 
 
 if __name__ == '__main__':
