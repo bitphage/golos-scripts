29e3cfb542342ee72e07c9cbf2a1541ea4a9c366
Vladimir Kamarzin <vvk@vvk.pp.ru>
2020-04-22 20:15:14 +0500

    Refactor sea_biom.py

--- sea_biom.py
+++ sea_biom.py
@@ -1,41 +1,18 @@
 #!/usr/bin/env python
 
-import argparse
-import logging
-import yaml
-from golos import Steem
-from golos.converter import Converter
+import click
+
+from golosscripts.decorators import common_options, helper
 
 
-log = logging.getLogger('functions')
+@click.command()
+@common_options
+@helper
+@click.pass_context
+def main(ctx):
+    """Print Golos Power for each sea habitant level."""
 
-
-def main():
-
-    parser = argparse.ArgumentParser(
-        description='', epilog='Report bugs to: https://github.com/bitfag/golos-scripts/issues'
-    )
-    parser.add_argument('-d', '--debug', action='store_true', help='enable debug output'),
-    parser.add_argument('-c', '--config', default='./common.yml', help='specify custom path for config file')
-    args = parser.parse_args()
-
-    # create logger
-    if args.debug is True:
-        log.setLevel(logging.DEBUG)
-    else:
-        log.setLevel(logging.INFO)
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
-    handler.setFormatter(formatter)
-    log.addHandler(handler)
-
-    # parse config
-    with open(args.config, 'r') as ymlfile:
-        conf = yaml.safe_load(ymlfile)
-
-    golos = Steem(nodes=conf['nodes_old'], keys=conf['keys'])
-    cv = Converter(golos)
-    spmv = cv.steem_per_mvests()
+    spmv = ctx.helper.converter.steem_per_mvests()
 
     print('fish: {:.0f}'.format(spmv * 1))
     print('dolphin: {:.0f}'.format(spmv * 10))
