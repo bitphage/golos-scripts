83fb20325c4066afe4b2653fe7bbb952481996a8
Vladimir Kamarzin <vvk@vvk.pp.ru>
2020-04-22 23:11:26 +0500

    Refactor estimate_median_price.py

--- estimate_median_price.py
+++ estimate_median_price.py
@@ -1,53 +1,25 @@
 #!/usr/bin/env python
 
-import sys
-import json
-import argparse
-import logging
-import yaml
-from golos import Steem
-from golos.amount import Amount
-from pprint import pprint
+import click
 
-import functions
-
-log = logging.getLogger('functions')
+from golosscripts.decorators import common_options, helper
 
 
-def main():
+@click.command()
+@common_options
+@helper
+@click.pass_context
+def main(ctx):
+    """Get witnesses price feed and estimate futher median price."""
 
-    parser = argparse.ArgumentParser(
-        description='Get witnesses price feed and estimate futher median price',
-        epilog='Report bugs to: https://github.com/bitfag/golos-scripts/issues',
-    )
-    parser.add_argument('-d', '--debug', action='store_true', help='enable debug output'),
-    parser.add_argument('-c', '--config', default='./common.yml', help='specify custom path for config file')
-    args = parser.parse_args()
+    ctx.log.info('current conversion price: {:.3f}'.format(ctx.helper.converter.sbd_median_price()))
+    ctx.log.info('estimated median price: {:.3f}'.format(ctx.helper.estimate_median_price()))
 
-    # create logger
-    if args.debug == True:
-        log.setLevel(logging.DEBUG)
-    else:
-        log.setLevel(logging.INFO)
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter("%(levelname)s: %(message)s")
-    handler.setFormatter(formatter)
-    log.addHandler(handler)
+    if ctx.debug:
+        feeds = ctx.helper.get_price_feeds()
 
-    # parse config
-    with open(args.config, 'r') as ymlfile:
-        conf = yaml.safe_load(ymlfile)
-
-    golos = Steem(nodes=conf['nodes_old'], keys=conf['keys'])
-
-    if args.debug:
-        verbose = True
-    else:
-        verbose = False
-
-    log.info('current median price: {:.3f}'.format(functions.get_median_price(golos)))
-    log.info('estimated median price: {:.3f}'.format(functions.estimate_median_price(golos, verbose=verbose)))
-    log.info('estimated median price (from feed): {:.3f}'.format(functions.estimate_median_price_from_feed(golos)))
+        for feed in feeds:
+            ctx.log.debug('{}: {:.3f}'.format(feed.owner, feed.price))
 
 
 if __name__ == '__main__':
