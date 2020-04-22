f1adb14ce850041469051a159800368b193ac368
Vladimir Kamarzin <vvk@vvk.pp.ru>
2020-04-22 21:39:21 +0500

    Refactor get_conversion_requests.py

--- get_conversion_requests.py
+++ get_conversion_requests.py
@@ -1,76 +1,47 @@
 #!/usr/bin/env python
 
 import sys
-import json
-import argparse
-import logging
-import yaml
-
-from pprint import pprint
-from decimal import Decimal
-from datetime import timedelta
 from datetime import datetime
+from decimal import Decimal
 
-from golos import Steem
-from golos.account import Account
-from golos.amount import Amount
+import click
+from golos.utils import parse_time
 
-import functions
-
-log = logging.getLogger('functions')
+from golosscripts.decorators import common_options, helper
 
 
-def main():
+@click.command()
+@common_options
+@helper
+@click.option('-n', '--notify', default=False, is_flag=True, help='send message to accounts who uses conversions')
+@click.option('-a', '--account', type=str, help='get requests for this single account')
+@click.pass_context
+def main(ctx, notify, account):
+    """Find all GBG conversion requests."""
 
-    parser = argparse.ArgumentParser(
-        description='Find all GBG conversion requests',
-        epilog='Report bugs to: https://github.com/bitfag/golos-scripts/issues',
-    )
-    parser.add_argument('-d', '--debug', action='store_true', help='enable debug output'),
-    parser.add_argument('-c', '--config', default='./common.yml', help='specify custom path for config file')
-    parser.add_argument('-n', '--notify', action='store_true', help='send message to accounts who uses conversions')
-    parser.add_argument('-a', '--account', help='get request for single account')
-    args = parser.parse_args()
-
-    # create logger
-    if args.debug == True:
-        log.setLevel(logging.DEBUG)
+    if account:
+        accs = [account]
     else:
-        log.setLevel(logging.INFO)
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
-    handler.setFormatter(formatter)
-    log.addHandler(handler)
+        ctx.log.debug('total accounts: %s', ctx.helper.get_account_count())
+        accs = ctx.helper.get_all_usernames()
 
-    # parse config
-    with open(args.config, 'r') as ymlfile:
-        conf = yaml.safe_load(ymlfile)
-
-    golos = Steem(nodes=conf['nodes_old'], keys=conf['keys'])
-
-    if args.account:
-        accs = [args.account]
-    else:
-        c = golos.get_account_count()
-        log.debug('total accounts: {}'.format(c))
-        accs = golos.get_all_usernames()
-
-    # obtain median and market prices whether we're going to send a notification
-    if args.notify:
-        bid = functions.get_market_price(golos)
-        median = functions.get_median_price(golos)
-        if not bid or not median:
-            log.critical('failed to obtain price')
+    # obtain conversion_price and market prices whether we're going to send a notification
+    if notify:
+        bid = ctx.helper.get_market_price(type_='bid')
+        conversion_price = ctx.helper.get_conversion_price()
+        if not bid or not conversion_price:
+            ctx.log.critical('failed to obtain price')
             sys.exit(1)
 
     start = datetime.utcnow()
     total_sum = Decimal('0.000')
     for acc in accs:
-        requests = golos.get_conversion_requests(acc)
+        requests = ctx.helper.get_conversion_requests(acc)
         total = Decimal('0.000')
         # Add datetime field
         for request in requests:
-            request['date'] = datetime.strptime(request['conversion_date'], '%Y-%m-%dT%H:%M:%S')
+            request['date'] = parse_time(request['conversion_date'])
+
         # Sort by datetime
         requests = sorted(requests, key=lambda k: k['date'])
         for request in requests:
@@ -87,12 +58,12 @@ def main():
         if len(requests) > 1:
             print('{:<16} {:<18} {:<7}'.format(request['owner'], total, 'Total'))
 
-        if requests and args.notify:
-            msg = conf['notify_message'].format(median, bid)
-            functions.transfer(golos, conf['notify_account'], acc, '0.001', 'GOLOS', msg)
+        if requests and notify:
+            msg = ctx.config['notify_message'].format(conversion_price, bid)
+            ctx.helper.transfer(acc, '0.001', 'GOLOS', memo=msg, account=ctx.config['notify_account'])
 
     print('Total on conversion: {}'.format(total_sum))
-    log.debug('getting conversion requests took {:.2f} seconds'.format((datetime.utcnow() - start).total_seconds()))
+    ctx.log.debug('getting conversion requests took {:.2f} seconds'.format((datetime.utcnow() - start).total_seconds()))
 
 
 if __name__ == '__main__':
