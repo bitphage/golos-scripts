0ea8e70e4e242912533e3a0d5f44d7e8351fc325
Vladimir Kamarzin <vvk@vvk.pp.ru>
2020-04-22 19:15:36 +0500

    Refactor get_account.py

--- get_account.py
+++ get_account.py
@@ -1,44 +1,24 @@
 #!/usr/bin/env python
 
 import json
-import argparse
-import logging
-import yaml
-from golos import Steem
+
+import click
 from golos.account import Account
 
+from golosscripts.decorators import common_options, helper
 
-log = logging.getLogger('functions')
 
+@click.command()
+@common_options
+@helper
+@click.argument('account')
+@click.pass_context
+def main(ctx, account):
+    """Show account object."""
 
-def main():
-
-    parser = argparse.ArgumentParser(
-        description='', epilog='Report bugs to: https://github.com/bitfag/golos-scripts/issues'
-    )
-    parser.add_argument('-d', '--debug', action='store_true', help='enable debug output'),
-    parser.add_argument('-c', '--config', default='./common.yml', help='specify custom path for config file')
-    parser.add_argument('account', help='account name')
-    args = parser.parse_args()
-
-    # create logger
-    if args.debug is True:
-        log.setLevel(logging.DEBUG)
-    else:
-        log.setLevel(logging.INFO)
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
-    handler.setFormatter(formatter)
-    log.addHandler(handler)
-
-    # parse config
-    with open(args.config, 'r') as ymlfile:
-        conf = yaml.safe_load(ymlfile)
-
-    golos = Steem(nodes=conf['nodes_old'], keys=conf['keys'])
-    a = Account(args.account, steemd_instance=golos)
-    j = json.dumps(a, indent=4)
-    print(j)
+    acc = Account(account)
+    js = json.dumps(acc, indent=4)
+    print(js)
 
 
 if __name__ == '__main__':
