358029da2f5dba77e3d90425046bb44711804f6f
Vladimir Kamarzin <vvk@vvk.pp.ru>
2020-04-22 20:49:51 +0500

    Refactor witness_approve.py

--- witness_approve.py
+++ witness_approve.py
@@ -1,44 +1,25 @@
 #!/usr/bin/env python
 
-import sys
-import json
-import argparse
-import logging
-import yaml
-from golos import Steem
+import click
 
-import functions
-
-log = logging.getLogger('functions')
+from golosscripts.decorators import common_options, helper
 
 
-def main():
+@click.command()
+@common_options
+@helper
+@click.argument('account')
+@click.argument('witness')
+@click.pass_context
+def main(ctx, account, witness):
+    """
+    Vote for witness.
 
-    parser = argparse.ArgumentParser(
-        description='Vote for witness', epilog='Report bugs to: https://github.com/bitfag/golos-scripts/issues'
-    )
-    parser.add_argument('-d', '--debug', action='store_true', help='enable debug output'),
-    parser.add_argument('-c', '--config', default='./common.yml', help='specify custom path for config file')
-    parser.add_argument('account', help='account name to use for voting'),
-    parser.add_argument('witness', help='witness name to approve')
-    args = parser.parse_args()
-
-    # create logger
-    if args.debug == True:
-        log.setLevel(logging.DEBUG)
-    else:
-        log.setLevel(logging.INFO)
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
-    handler.setFormatter(formatter)
-    log.addHandler(handler)
-
-    # parse config
-    with open(args.config, 'r') as ymlfile:
-        conf = yaml.safe_load(ymlfile)
-
-    golos = Steem(nodes=conf['nodes_old'], keys=conf['keys'])
-    golos.approve_witness(args.witness, account=args.account)
+    \b
+    ACCOUNT - account name to use for voting
+    WITNESS - witness name to approve
+    """
+    ctx.helper.approve_witness(witness, account=account)
 
 
 if __name__ == '__main__':
