377f2cde6433ff0210fb81d8bcd62b89d93fd227
Vladimir Kamarzin <vvk@vvk.pp.ru>
2020-04-22 20:22:52 +0500

    Refactor transfer_to_vesting.py

--- transfer_to_vesting.py
+++ transfer_to_vesting.py
@@ -1,51 +1,21 @@
 #!/usr/bin/env python
 
-import sys
-import json
-import argparse
-import logging
-import yaml
-import traceback
-from golos import Steem
+import click
 
-log = logging.getLogger(__name__)
+from golosscripts.decorators import common_options, helper
 
 
-def main():
+@click.command()
+@common_options
+@helper
+@click.argument('from_')
+@click.argument('to')
+@click.argument('amount', type=float)
+@click.pass_context
+def main(ctx, from_, to, amount):
+    """Transfer GOLOS to GOLOS POWER."""
 
-    parser = argparse.ArgumentParser(
-        description='transfer GOLOS to GOLOS POWER',
-        epilog='Report bugs to: https://github.com/bitfag/golos-scripts/issues',
-    )
-    parser.add_argument('-d', '--debug', action='store_true', help='enable debug output'),
-    parser.add_argument('--broadcast', action='store_true', default=False, help='broadcast transactions'),
-    parser.add_argument('-c', '--config', default='./common.yml', help='specify custom path for config file')
-    parser.add_argument('f', help='from'),
-    parser.add_argument('to', help='to'),
-    parser.add_argument('amount', help='amount'),
-    args = parser.parse_args()
-
-    # create logger
-    if args.debug == True:
-        log.setLevel(logging.DEBUG)
-    else:
-        log.setLevel(logging.INFO)
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
-    handler.setFormatter(formatter)
-    log.addHandler(handler)
-
-    # parse config
-    with open(args.config, 'r') as ymlfile:
-        conf = yaml.safe_load(ymlfile)
-
-    # initialize steem instance
-    log.debug('broadcast: %s', args.broadcast)
-    # toggle args.broadcast
-    b = not args.broadcast
-    golos = Steem(nodes=conf['nodes_old'], no_broadcast=b, keys=conf['keys'])
-
-    golos.transfer_to_vesting(args.amount, to=args.to, account=args.f)
+    ctx.helper.transfer_to_vesting(amount, to=to, account=from_)
 
 
 if __name__ == '__main__':
