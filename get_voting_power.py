f1786f58678bcb9fd165e895c09ad305e135170e
Vladimir Kamarzin <vvk@vvk.pp.ru>
2020-04-22 23:21:46 +0500

    Refactor get_voting_power.py

--- get_voting_power.py
+++ get_voting_power.py
@@ -1,46 +1,20 @@
 #!/usr/bin/env python
 
-import sys
-import json
-import argparse
-import logging
-import yaml
-from golos import Steem
+import click
 
-import functions
-
-log = logging.getLogger('functions')
+from golosscripts.decorators import common_options, helper
 
 
-def main():
+@click.command()
+@common_options
+@helper
+@click.argument('account')
+@click.pass_context
+def main(ctx, account):
+    """Calculate actual voting power of an account."""
 
-    parser = argparse.ArgumentParser(
-        description='Calculate actual voting power',
-        epilog='Report bugs to: https://github.com/bitfag/golos-scripts/issues',
-    )
-    parser.add_argument('-d', '--debug', action='store_true', help='enable debug output'),
-    parser.add_argument('-c', '--config', default='./common.yml', help='specify custom path for config file')
-    parser.add_argument('account', help='specify account which VP should be estimated')
-
-    args = parser.parse_args()
-
-    # create logger
-    if args.debug == True:
-        log.setLevel(logging.DEBUG)
-    else:
-        log.setLevel(logging.INFO)
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter("%(levelname)s: %(message)s")
-    handler.setFormatter(formatter)
-    log.addHandler(handler)
-
-    # parse config
-    with open(args.config, 'r') as ymlfile:
-        conf = yaml.safe_load(ymlfile)
-
-    golos = Steem(nodes=conf['nodes_old'], keys=conf['keys'])
-    vp = functions.get_voting_power(golos, args.account)
-    log.info('VP of {}: {:.2f}'.format(args.account, vp))
+    vp = ctx.helper.get_voting_power(account)
+    ctx.log.info('VP of {}: {:.2f}'.format(account, vp))
 
 
 if __name__ == '__main__':
