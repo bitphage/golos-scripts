6fbda77fad25d7cc681761b1d8308dfc92c74066
Vladimir Kamarzin <vvk@vvk.pp.ru>
2020-04-22 23:31:09 +0500

    Refactor calc_vesting_reward.py

--- calc_vesting_reward.py
+++ calc_vesting_reward.py
@@ -1,48 +1,25 @@
 #!/usr/bin/env python
 
-import argparse
-import logging
-import yaml
-
-from golos import Steem
+import click
 from golos.account import Account
 from golos.amount import Amount
 
+from golosscripts.decorators import common_options, helper
 
-log = logging.getLogger('functions')
 
-
-def main():
-
-    parser = argparse.ArgumentParser(
-        description='', epilog='Report bugs to: https://github.com/bitfag/golos-scripts/issues'
-    )
-    parser.add_argument('-d', '--debug', action='store_true', help='enable debug output'),
-    parser.add_argument('-c', '--config', default='./common.yml', help='specify custom path for config file')
-    parser.add_argument('account')
-    args = parser.parse_args()
-
-    # create logger
-    if args.debug is True:
-        log.setLevel(logging.DEBUG)
-    else:
-        log.setLevel(logging.INFO)
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
-    handler.setFormatter(formatter)
-    log.addHandler(handler)
-
-    # parse config
-    with open(args.config, 'r') as ymlfile:
-        conf = yaml.safe_load(ymlfile)
-
-    golos = Steem(nodes=conf['nodes_old'], keys=conf['keys'])
-    props = golos.get_dynamic_global_properties()
-    a = Account(args.account, steemd_instance=golos)
-    b = a.get_balances()
-    account_vesting = b['total']['GESTS']
+@click.command()
+@common_options
+@helper
+@click.argument('account')
+@click.pass_context
+def main(ctx, account):
+    """Calculate profit from vesting holdings."""
+    props = ctx.helper.get_dynamic_global_properties()
+    acc = Account(account)
+    balance = acc.get_balances()
+    account_vesting = balance['total']['GESTS']
     vesting_fund = Amount(props['total_vesting_shares'])
-    log.info('%s vesting share: %s', args.account, account_vesting / vesting_fund.amount)
+    ctx.log.info('%s vesting share: %s', account, account_vesting / vesting_fund.amount)
 
 
 if __name__ == '__main__':
