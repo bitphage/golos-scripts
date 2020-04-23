8d1f4d0f10346836986e6c45c04c7e4a674d692a
Vladimir Kamarzin <vvk@vvk.pp.ru>
2020-04-23 11:13:50 +0500

    Fix donation.py

--- donation.py
+++ donation.py
@@ -10,18 +10,17 @@ from golosscripts.decorators import common_options, helper
 @click.command()
 @common_options
 @helper
-@click.option('--broadcast', default=False, is_flag=True, help='broadcast transaction')
 @click.argument('from_')
 @click.argument('amount', type=float)
 @click.argument('asset')
 @click.argument('url')
 @click.pass_context
-def main(ctx, from_, to, amount, asset, url):
+def main(ctx, from_, amount, asset, url):
     """Make a donation in liquid asset for specific post."""
 
     post = ctx.helper.parse_url(url)
     memo = {'donate': {'post': '/@{}/{}'.format(post.author, post.permlink)}}
-    ctx.log.info('transfer {} -> {}: {} {}'.format(from_, to, amount, asset))
+    ctx.log.info('transfer {} -> {}: {} {}'.format(from_, post.author, amount, asset))
     ctx.helper.transfer(post.author, amount, asset, memo=json.dumps(memo), account=from_)
 
 
