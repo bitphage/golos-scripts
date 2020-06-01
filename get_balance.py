464c5299960b78f72a64f8d54f814847ff9f9f85
Vladimir Kamarzin <vvk@vvk.pp.ru>
2020-06-01 23:23:36 +0500

    Add tip and claim balances

--- get_balance.py
+++ get_balance.py
@@ -16,13 +16,17 @@ def main(ctx, account):
 
     account = Account(account)
     balance = account.get_balances()
-    gp = ctx.helper.converter.vests_to_sp(balance['total']['GESTS'])
+
+    print('{:<15}{:>18.3f}'.format('To claim GOLOS:', balance['accumulative']['GOLOS']))
+    print('{:<15}{:>18.3f}'.format('Tip GOLOS:', balance['tip']['GOLOS']))
 
     for asset in balance['savings']:
         print('{:<15}{:>18.3f}'.format('SAVINGS_{}'.format(asset), balance['savings'][asset]))
-    for asset in balance['total']:
+
+    for asset in balance['available']:
         print('{:<15}{:>18.3f}'.format('{}:'.format(asset), balance['available'][asset]))
 
+    gp = ctx.helper.converter.vests_to_sp(balance['total']['GESTS'])
     print('{:<15}{:>18.3f}'.format('GP:', gp))
     print('{:<15}{:>18.3f}'.format('MGESTS:', balance['total']['GESTS'] / 1000000))
 
